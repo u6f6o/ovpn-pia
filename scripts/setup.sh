83cc492b6e222eea21702addaa9e5122edc7013e
Ulf Gitschthaler <ulf@gitschthaler.de>
2016-09-09 23:53:06 +0200

    remove duplicate checks

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,23 +4,6 @@
 curl -OLk https://www.privateinternetaccess.com/openvpn/openvpn.zip 
 unzip -qq openvpn.zip
 
-# some basic checks
-
-if [ `ls *.ovpn | wc -l` -lt 20 ]; then 
-	echo "Openvpn config files missing after extraction."
-	exit 1 
-fi
-
-if [ `ls *.pem | wc -l` -ne 1 ]; then 
-	echo "Openvpn pem file missing after extraction."
-	exit 1
-fi
-
-if [ `ls *.crt | wc -l` -ne 1 ]; then 
-	echo "Openvpn crt file missing after extraction."
-	exit 1
-fi
-
 # change credentials, provide absolute paths
 
 sed -i 's/\(auth-user-pass\)/\1 USER PW/g' *.ovpn
@@ -38,7 +21,7 @@ done
 cp -v *.pem /etc/openvpn || exit 1
 cp -v *.crt /etc/openvpn || exit 1
 
-# basic checks again
+# basic checks for openvpn config and cert files
 
 if [ `ls /etc/openvpn/*.ovpn | wc -l` -lt 20 ]; then 
 	echo "Openvpn config files missing after copy."
