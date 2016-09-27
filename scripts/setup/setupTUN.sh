9aed9ae39f2fc9beb90fef432d70fc793ddb563b
Ulf Gitschthaler <ulf@gitschthaler.de>
2016-09-27 23:04:27 +0200

    clean a bit

--- scripts/setup/setupTUN.sh
+++ scripts/setup/setupTUN.sh
@@ -1,23 +1,25 @@
 #!/bin/bash 
 
-# create tun device
-#[ -d /dev/net ] || mkdir -p /dev/net
-#[ -c /dev/net/tun ] || mknod /dev/net/tun c 10 200
-#chown openvpn:openvpn /dev/net/tun 
-#chmod 666 /dev/net/tun
-
 # add tun to modules
 echo "tun" >> /etc/modules 
 
 # create unpriv-ip 
 mkdir -p /usr/local/sbin/
-echo "#!/bin/sh" >> /usr/local/sbin/unpriv-ip
-echo "sudo /sbin/ip \$*" >> /usr/local/sbin/unpriv-ip
 
-# allow sudo call 
-echo "openvpn ALL=(ALL) NOPASSWD: /sbin/ip" >> /etc/sudoers
-echo "Defaults:openvpn !requiretty" >> /etc/sudoers
+cat <<EOF >> /usr/local/sbin/unpriv-ip
+#!/bin/sh
+sudo /sbin/ip \$*
+EOF
 
-chmod 755 /usr/local/sbin/unpriv-ip
-chown openvpn:openvpn /etc/openvpn -R
- 
+chown openvpn:openvpn /usr/local/sbin/unpriv-ip
+chmod ugo-rwx /usr/local/sbin/unpriv-ip
+chmod ug+rx /usr/local/sbin/unpriv-ip
+
+# allow openvpn to execute /sbin/ip
+cat <<EOF >> /etc/sudoers
+openvpn ALL=(ALL) NOPASSWD: /sbin/ip
+Defaults:openvpn !requiretty
+EOF
+
+chown openvpn:openvpn /etc/openvpn -R 
+sed -i 's/^\(netdev:.\+\)$/\1openvpn/' /etc/group
